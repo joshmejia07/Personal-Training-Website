6d14735d0202e747145d17e977333a0adc29a64a
joshmejia07 <113376157+joshmejia07@users.noreply.github.com>
2024-07-11 09:41:47 -0700

    Hero Styling Update

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -3,48 +3,13 @@ import { useMediaQuery } from "react-responsive"
 import hero from "../../assets/images/hero-img.jpg"
 
 export default function Hero() {
-  const isBigScreen = useMediaQuery({ query: "(min-width: 82em)" })
-
-  const mediaQuery = (
-    <>
-      {isBigScreen ? (
-        <div className="hero-widescreen">
-          <div className="hero-text-bg">
-            <div className="hero-text-container">
-              <h4 className="text-colored">train with</h4>
-              <h1>MOMNTM</h1>
-              <hr className="left" />
-              <p>Personal training. Nutritional coaching. Fitness education.</p>
-              <Link to="/programs">
-                <button>See Programs</button>
-              </Link>
-            </div>
-          </div>
-          <img className="hero-img-widescreen" src={hero} />
-        </div>
-      ) : (
-        <div className="hero-img">
-          <div className="hero-text-container">
-            <h4 className="text-colored">train with</h4>
-            <h1>MOMNTM</h1>
-            <hr className="left" />
-            <p>Personal training. Nutritional coaching. Fitness education.</p>
-            <Link to="/programs">
-              <button>See Programs</button>
-            </Link>
-          </div>
-        </div>
-      )}
-    </>
-  )
-
   return (
     <div className="hero-img">
       <div className="hero-text-container">
-        <h4 className="text-colored">train with</h4>
+        {/* <h4 className="text-colored">train with</h4> */}
         <h1>MOMNTM</h1>
-        <hr className="left" />
-        <p>Personal training. Nutritional coaching. Fitness education.</p>
+        {/* <hr className="left" /> */}
+        {/* <p>Personal training. Nutritional coaching. Fitness education.</p> */}
         <Link to="/programs">
           <button>See Programs</button>
         </Link>
