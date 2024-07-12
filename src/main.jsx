cb78ccd568cff7862252655c60a8ef31eebeb318
joshmejia07 <113376157+joshmejia07@users.noreply.github.com>
2024-07-11 17:41:56 -0700

    Update name & router base

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/momntm">
       <ScrollToTop />
       <App />
     </Router>
